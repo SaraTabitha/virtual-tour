5ceb967ce6a7287b96cbf95a3e001e01c0157bc7
SaraTabitha <mayhes06@uwosh.edu>
2017-05-18 15:39:06 -0500

    moved styling and scripts to their own respective files that are linked to index.html

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -73,4 +73,247 @@
   }
 
   // Your custom JavaScript goes here
+
+
+  // function for hiding all of the cards
+  function hideAll(){
+    $("#athCard").hide();
+    $("#accSCard").hide();
+    $("#campSCard").hide();
+    $("#accFCard").hide();
+    $("#resHCard").hide();
+    $("#diningCard").hide();
+    $("#stuRCard").hide();
+  }
+
+  // info bulding card slide in from left (when blue link is clicked)
+  // academic facilities show and slide out
+  $("#accFText").click(function(){
+    hideAll();
+    $("#accFCard").show();
+    $("#accFCard").animate(
+      {left: "377px"});
+  });
+  // click x button on academic facilities card and it will disappear again
+  $("#accFClose").click(function(){
+    $("#accFCard").animate(
+      {left:"-377px"});
+  });
+  // end academic facilities
+  //
+  // athletics show and slide out
+  $("#athText").click(function(){
+    hideAll();
+    $("#athCard").show();
+    $("#athCard").animate(
+      {left: "377px"});
+  });
+  // athletics x button hide
+  $("#athClose").click(function(){
+    $("#athCard").animate(
+      {left:"-377px"});
+  });
+  // end athletics
+  //
+  // start academic services show and slide out
+  $("#accSText").click(function(){
+    hideAll();
+    $("#accSCard").show();
+    $("#accSCard").animate(
+      {left: "377px"});
+  });
+  // academic services x button hide
+  $("#accSClose").click(function(){
+    $("#accSCard").animate(
+      {left:"-377px"});
+  });
+  // end accademic services
+  //
+  // start campus services show and slide out
+  $("#campSText").click(function(){
+    hideAll();
+    $("#campSCard").show();
+    $("#campSCard").animate(
+      {left: "377px"});
+  });
+  //  campus services x button hide
+  $("#campSClose").click(function(){
+    $("#campSCard").animate(
+      {left:"-377px"});
+  });
+  // end campus services
+  //
+  // start Residence halls show and slide out
+  $("#resHText").click(function(){
+    hideAll();
+    $("#resHCard").show();
+    $("#resHCard").animate(
+      {left: "377px"});
+  });
+  // Residence halls x button hide
+  $("#resHClose").click(function(){
+    $("#resHCard").animate(
+      {left:"-377px"});
+  });
+  // end residence halls
+  //
+  // start Dining show and slide out
+  $("#diningText").click(function(){
+    hideAll();
+    $("#diningCard").show();
+    $("#diningCard").animate(
+      {left: "377px"});
+  });
+  // Dining x button hide
+  $("#diningClose").click(function(){
+    $("#diningCard").animate(
+      {left:"-377px"});
+  });
+  // end Dining
+  //
+  // start Student Recreation show and slide out
+  $("#stuRText").click(function(){
+    hideAll();
+    $("#stuRCard").show();
+    $("#stuRCard").animate(
+      {left: "377px"});
+  });
+  // Student Recreation x button hide
+  $("#stuRClose").click(function(){
+    $("#stuRCard").animate(
+      {left:"-377px"});
+  });
+  // end Student Recreation
+  //
+
+
+  // Start hide/show 360 navigation items
+  $("#linkOne").click(function(){
+    // div toggle
+    $("#linkOneOptions").toggle("slow");
+    // + symbol toggle
+    $("#addOne").toggle();
+  });
+  $("#linkTwo").click(function(){
+    $("#linkTwoOptions").toggle("slow");
+    $("#addTwo").toggle();
+  });
+  $("#linkThree").click(function(){
+    $("#linkThreeOptions").toggle("slow");
+    $("#addThree").toggle();
+  });
+  // End hide/show 360 navigation items
+
+    // event listener for the select all button attached to function clicking
+    $("#selectallcheck").click(function(){
+
+      //checks the input for the checked property (! = not checked)
+      var check = !$("#selectallcheck").prop("checked");
+    // because it looks at whether or not the property is checked BEFORE The click is put into effect,
+    //it has to do the opposite of what the state of the click is ex. If check was false before click
+    // do: check everything (and then the select all with check as the very last step (and become true))
+      if(check === false){
+      // buildings
+      $("#buildings").prop("checked", true);
+      $("#buildLabel").addClass("is-checked");
+
+      // parking lots
+      $("#parkingLots").prop("checked", true);
+      $("#parkingLabel").addClass("is-checked");
+      // accessible entries
+      $("#accEnt").prop("checked", true);
+      $("#accEntLabel").addClass("is-checked");
+      // Accessible Parking
+      $("#accPar").prop("checked", true);
+      $("#accParLabel").addClass("is-checked");
+      // Emergency Phones
+      $("#emergency").prop("checked", true);
+      $("#emergencyLabel").addClass("is-checked");
+      // sustainability points of interest
+      $("#sust").prop("checked", true);
+      $("#sustLabel").addClass("is-checked");
+      // gender neutral and family bathrooms
+      $("#gender").prop("checked", true);
+      $("#genderLabel").addClass("is-checked");
+
+      } else if (check === true) {
+
+      // buildings
+      $("#buildings").removeProp("checked", true);
+      $("#buildLabel").removeClass("is-checked");
+
+      // parking lots
+      $("#parkingLots").removeProp("checked", true);
+      $("#parkingLabel").removeClass("is-checked");
+      // Accessible entries
+      $("#accEnt").removeProp("checked", true);
+      $("#accEntLabel").removeClass("is-checked");
+      // Accessible Parking
+      $("#accPar").removeProp("checked", true);
+      $("#accParLabel").removeClass("is-checked");
+      // Emergency Phones
+      $("#emergency").removeProp("checked", true);
+      $("#emergencyLabel").removeClass("is-checked");
+      // sustainability points of interest
+      $("#sust").removeProp("checked", true);
+      $("#sustLabel").removeClass("is-checked");
+      // gender neutral and family bathrooms
+      $("#gender").removeProp("checked", true);
+      $("#genderLabel").removeClass("is-checked");
+
+      }
+    });
+
+
+  // Start second Select All (identical to above so I will omit comments explaining functionality)
+
+  $("#selectallcheck2").click(function(){
+    var checkTwo = !$("#selectallcheck2").prop("checked");
+    if(checkTwo === false){
+      // academic facilities
+      $("#accaF").prop("checked",true);
+      $("#accaFLabel").addClass("is-checked");
+      // athletics
+      $("#ath").prop("checked",true);
+      $("#athLabel").addClass("is-checked");
+      // academic services
+      $("#accS").prop("checked",true);
+      $("#accSLabel").addClass("is-checked");
+      // campus services
+      $("#campS").prop("checked",true);
+      $("#campSLabel").addClass("is-checked");
+      // residence halls
+      $("#resH").prop("checked",true);
+      $("#resHLabel").addClass("is-checked");
+      // dining
+      $("#dining").prop("checked",true);
+      $("#diningLabel").addClass("is-checked");
+      // student recreation
+      $("#stuR").prop("checked",true);
+      $("#stuRLabel").addClass("is-checked");
+  } else if (checkTwo === true) {
+    // academic facilities
+    $("#accaF").removeProp("checked", true);
+    $("#accaFLabel").removeClass("is-checked");
+    // athletics
+    $("#ath").removeProp("checked", true);
+    $("#athLabel").removeClass("is-checked");
+    // academic services
+    $("#accS").removeProp("checked", true);
+    $("#accSLabel").removeClass("is-checked");
+    // campus services
+    $("#campS").removeProp("checked", true);
+    $("#campSLabel").removeClass("is-checked");
+    // residence halls
+    $("#resH").removeProp("checked", true);
+    $("#resHLabel").removeClass("is-checked");
+    // dining
+    $("#dining").removeProp("checked", true);
+    $("#diningLabel").removeClass("is-checked");
+    // student recreation
+    $("#stuR").removeProp("checked", true);
+    $("#stuRLabel").removeClass("is-checked");
+  }
+});
+  // End second Select All
 })();
