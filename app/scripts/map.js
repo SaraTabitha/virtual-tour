b6c306a342a2b1fe6cefb8eeef3d9e870baf8406
SaraTabitha <mayhes06@uwosh.edu>
2017-09-22 13:27:32 -0500

    moved all of buttons out of title line and modified css to look properly on mobile with scaling

--- app/scripts/map.js
+++ app/scripts/map.js
@@ -506,6 +506,7 @@ function initMap() {
         });
         // when x on popup is clicked, close popup
         $("#albeePopupClose").click(function(){
+            
             $('#overlay').animate({"opacity": "0"}, "slow");
             $('#overlay').css('visibility', 'hidden');
 
