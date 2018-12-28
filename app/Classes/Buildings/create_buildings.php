8e68feb5444a59ecb81c5adce71faef9292b2ad6
SaraTabitha <mayhes06@uwosh.edu>
2018-12-28 09:56:57 -0600

    fleshed out create_buildings; rewrote style_checkboxes

--- app/Classes/Buildings/create_buildings.php
+++ app/Classes/Buildings/create_buildings.php
@@ -2,9 +2,18 @@
 require_once("Building.php");
 require_once("php/contactRestAPI.php");
 
-$buildings = connectRestAPI("https://wwwtest.uwosh.edu/virtual-tour-cms/wp-json/wp/v2/buildings?per_page=200"); //per_page=100: this will return the first 100 results; if there are ever more than 100 buildings increase this number
+$buildings = connectRestAPI("https://wwwtest.uwosh.edu/virtual-tour-cms/wp-json/wp/v2/buildings?per_page=100"); //per_page=100: this will return the first 100 results
+$media = connectRestAPI("https://wwwtest.uwosh.edu/virtual-tour-cms/wp-json/wp/v2/media?per_page=100");
 
-foreach($buildings as $item){
+
+$building_list = array();
+$building_markers = array();
+
+$accessibleBuilding_markers = array();
+$sustainableBuilding_markers = array();
+$bathroomBuilding_markers = array();
+
+foreach($buildings as $index=>$item){
     $slug = $item->slug;
     $title = $item->title;
     $isAccessible = $item->is_accessible;
@@ -12,8 +21,8 @@ foreach($buildings as $item){
     $city = $item->address->city;
     $state = $item->address->state;
     $zipcode = $item->address->zip;
-    $full_image_num = $item->full_image; //need to get image url
-    $thumb_image_num = $item->tooltip_image; //need to get image url 
+    $full_image_index= $item->full_image; //need to get image url
+    $thumb_image_index = $item->tooltip_image; //need to get image url 
     $building_categories = $item->building_categories;
     $latitude = $item->location->latitude; //turn into marker
     $longitude = $item->location->longitude; //turn into marker
@@ -23,6 +32,48 @@ foreach($buildings as $item){
     $bathrooms_tab_content = $item->sustainability;
     $dining_tab_content = $item->dining;
 
+    $building_markers[$index] = new Marker($slug, $latitude, $longitude, $title);
+
+    //get full_image and thumb_image urls
+    $full_image = $media[$full_image_index]->guid->rendered;
+    $thumb_image = $media[$thumb_image_index]->guid->rendered;
+
+    $building_list = new Building($slug, $title, $isAccessible, $street, $city, $state, $zipcode, $full_image, $thumb_image,
+    $building_categories, $building_markers[$index], $about_tab_content, $tour_tab_content, $sustainability_tab_content, $dining_tab_content);
+
+    //markers for accessible entry markergroup
+    if($isAccessible){
+        array_push($accessibleBuilding_markers, $building_markers[$index]);
+    }
+    //markers for sustainability points of interest markergroup
+    if($tour_tab_content != null){
+        array_push($sustainableBuilding_markers, $building_markers[$index]);
+    }
+    //markers for genderneutral&family bathroom markergroup
+    if($bathrooms_tab_content != null){
+        array_push($bathroomBuilding_markers, $building_markers[$index]);
+    }
 }
+
+$slug = "buildings";
+$checkboxColor = "#fccd32";
+$markerIcon = "images/markers/yellow.png";
+$allBuildings = new MarkerGroup($slug, $checkboxColor, $markerIcon, $building_markers);
+
+$slug = "accEnt";
+$checkboxColor = "#eb212e";
+$markerIcon = "images/markers/red.png";
+$accessibleBuildings = new MarkerGroup($slug, $checkboxColor, $markerIcon, $accessibleBuilding_markers);;
+
+$slug = "sust";
+$checkboxColor = "#b3d143";
+$markerIcon = "images/markers/green.png";
+$sustainableBuildings = new MarkerGroup($slug, $checkboxColor, $markerIcon, $sustainabilityBuilding_markers);;
+
+$slug = "gender";
+$checkboxColor = "#c1a3cb";
+$markerIcon = "images/markers/lightpurple.png";
+$bathroomBuildings = new MarkerGroup($slug, $checkboxColor, $markerIcon, $bathroomBuilding_markers);;
+
 
 ?>
--- app/Classes/Marker_Groups/style_checkboxes.php
+++ app/Classes/Marker_Groups/style_checkboxes.php
@@ -5,9 +5,13 @@
     */
     require_once(__DIR__ . '/../Emergency_Phones/create_emergencyphones.php');
     require_once(__DIR__ . '/../Parking_Lots/create_parkinglots.php');
+    // require_once(__DIR__ . '/../Buildings/create_buildings.php');
 
-    $emergencyPhones->style_checkboxes();
-    $parkingLots->style_checkboxes();
-    $accessibleParkingLots->style_checkboxes();
+    // $markerGroups = array($emergencyPhones, $parkingLots, $accessibleParkingLots,
+    // $allBuildings, $accessibleBuildings, $sustainableBuildings, $bathroomBuildings);
+    $markerGroups = array($emergencyPhones, $parkingLots, $accessibleParkingLots);
+    foreach($markerGroups as $group){
+        $group->style_checkboxes();
+    }
 
 ?>
