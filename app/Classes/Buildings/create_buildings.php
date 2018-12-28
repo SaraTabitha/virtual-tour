abcd6610f6a82c54504977690dd80423a9c75269
SaraTabitha <mayhes06@uwosh.edu>
2018-12-28 11:03:12 -0600

    fixed getting image urls & null values passed to constructor

--- app/Classes/Buildings/create_buildings.php
+++ app/Classes/Buildings/create_buildings.php
@@ -13,16 +13,28 @@ $accessibleBuilding_markers = array();
 $sustainableBuilding_markers = array();
 $bathroomBuilding_markers = array();
 
+function getImageURL($id){
+    global $media;
+    $url; 
+
+    foreach($media as $image){
+        if($image->id == $id){
+            $url = $image->guid->rendered;
+        }
+    }
+    return $url;
+}
+
 foreach($buildings as $index=>$item){
     $slug = $item->slug;
-    $title = $item->title;
+    $title = $item->title->rendered;
     $isAccessible = $item->is_accessible;
     $street = $item->address->street; 
     $city = $item->address->city;
     $state = $item->address->state;
     $zipcode = $item->address->zip;
-    $full_image_index= $item->full_image; //need to get image url
-    $thumb_image_index = $item->tooltip_image; //need to get image url 
+    $full_image_id= $item->full_image; //need to get image url
+    $thumb_image_id = $item->tooltip_image; //need to get image url 
     $building_categories = $item->building_categories;
     $latitude = $item->location->latitude; //turn into marker
     $longitude = $item->location->longitude; //turn into marker
@@ -35,11 +47,10 @@ foreach($buildings as $index=>$item){
     $building_markers[$index] = new Marker($slug, $latitude, $longitude, $title);
 
     //get full_image and thumb_image urls
-    $full_image = $media[$full_image_index]->guid->rendered;
-    $thumb_image = $media[$thumb_image_index]->guid->rendered;
+    $full_image = getImageURL($full_image_id);
+    $thumb_image = getImageURL($thumb_image_id);
 
-    $building_list = new Building($slug, $title, $isAccessible, $street, $city, $state, $zipcode, $full_image, $thumb_image,
-    $building_categories, $building_markers[$index], $about_tab_content, $tour_tab_content, $sustainability_tab_content, $dining_tab_content);
+    
 
     //markers for accessible entry markergroup
     if($isAccessible){
@@ -53,6 +64,28 @@ foreach($buildings as $index=>$item){
     if($bathrooms_tab_content != null){
         array_push($bathroomBuilding_markers, $building_markers[$index]);
     }
+
+    /*
+    * constructor breaks if given a null value
+    */
+    if($about_tab_content == NULL){
+        $about_tab_content = false;
+    }
+    if($tour_tab_content == NULL){
+        $tour_tab_content = false;
+    }
+    if($sustainability_tab_content == NULL){
+        $sustainability_tab_content = false;
+    }
+    if($bathrooms_tab_content == NULL){
+        $bathrooms_tab_content = false;
+    }
+    if($dining_tab_content == NULL){
+        $dining_tab_content = false;
+    }
+
+    $building_list = new Building($slug, $title, $isAccessible, $street, $city, $state, $zipcode, $full_image, $thumb_image,
+    $building_categories, $building_markers[$index], $about_tab_content, $tour_tab_content, $sustainability_tab_content, $bathrooms_tab_content, $dining_tab_content);
 }
 
 $slug = "buildings";
